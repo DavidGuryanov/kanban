07d4bbca8d9def063ee42a0b9c9e49a6dc27ca3c
Гурьянов Давид Дмитриевич <davidguryanov@yandex.ru>
2021-12-24 20:43:15 +0300

    Minor fixes

--- src/App/styles.tsx
+++ src/App/styles.tsx
@@ -1,12 +1,18 @@
 import { styled } from "@linaria/react";
 
 export const BoardWrapper = styled.div`
-  background-color: #f8f8f8;
   font-family: "Poppins", sans-serif;
   color: #221c1d;
-  border-radius: 12px;
+  box-shadow: 0 2px 13px 7px rgb(78 78 78 / 29%);
   padding: 20px;
   min-height: 100%;
+  max-width: 1100px;
+  margin: 0 auto;
+  background-color: #e5e5e5;
+  opacity: 1;
+  background-image:  radial-gradient(#221c1d 0.7000000000000001px, transparent 0.7000000000000001px), radial-gradient(#221c1d 0.7000000000000001px, #e5e5e5 0.7000000000000001px);
+  background-size: 28px 28px;
+  background-position: 0 0,14px 14px;
 `;
 
 export const BoardTitle = styled.h1`
